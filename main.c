97a1a42ea9a389f42865e3ffe3950433a0686956
Asif Rot <asif.rot12@gmail.com>
2020-12-03 16:30:21 +0200

    update after testing

--- main.c
+++ main.c
@@ -1,62 +1,56 @@
 #include <stdio.h>
 #include "myBank.h"
 
-int main(){
-  char Transaction; 
-  while (1){
+int main()
+{
+  char Transaction = ' ';
+  
+  while (1)
+  {
     printf("\nPlease choose a transaction type: \n");
-    printf(" O-Open Account\n B-Balance Inquiry\n D-Deposit\n W-Withdrawal\n C-Close Account\n I-Interest\n P-Print\n E-Exit\n");
+    printf(" O-Open Account\n B-Balance Inquiry\n D-Deposit\n W-Withdraw\n C-Close Account\n I-Interest\n P-Print\n E-Exit\n");
     scanf(" %c", &Transaction);
-    if(Transaction=='O'){
-      double amount;
-      printf("Please enter amount for deposit: \n");
-      scanf("%lf", &amount);
-      openAccount(amount);
-    }
-    if(Transaction=='B'){
-      int accountNum;
-      printf("Please enter account number: \n");
-      scanf("%d", &accountNum);
-      checkBalance(accountNum);
-    }
-    if(Transaction=='D'){
-      int accountNum;
-      printf("Please enter account number: \n");
-      scanf("%d", &accountNum);
-      deposit(accountNum);
-    }
 
-    if(Transaction=='W'){
-      int accountNum;
-      printf("Please enter account number: \n");
-      scanf("%d", &accountNum);
-      withdraw(accountNum);
-    }
+    switch(Transaction) {
 
-    if(Transaction=='C'){
-      int accountNum;
-      printf("Please enter account number: \n");
-      scanf("%d", &accountNum);
-      closeAccount(accountNum);
-    }
-
-    if(Transaction=='I'){
-      addInterest();
-    }
-
-    if(Transaction=='P'){
-      printAccount();
-    }
-
-    if(Transaction=='E'){
-      closeAccounts();
+    case 'O':
+      openAccount();
       break;
+
+    case 'B':
+      checkBalance();
+      break;
+
+    case 'D':
+      deposit();
+      break;
+
+    case 'W':
+      withdraw();
+      break;
+
+    case 'C':
+      closeAccount();
+      break;
+
+    case 'I':
+      addInterest();
+      break;
+
+    case 'P':
+      printAccount();
+      break;
+
+    case 'E':
+      closeAccounts();
+      return 0;
+      break;
+
+    default:
+      printf("invalid command!\n");
+      break;
+
     }
-
-    if(Transaction!='O'&&Transaction!='B'&&Transaction!='D'&&Transaction!='W'&&Transaction!='C'&&Transaction!='I'&&Transaction!='P'&&Transaction!='E'){
-      printf("invaild command! \n");
   }
-  }
-
   return 0;
 }
--- myBank.c
+++ myBank.c
@@ -1,112 +1,210 @@
 #include <stdio.h>
+#include "myBank.h"
 double bank[3][50];
 
-void openAccount(double amount){
-    int i;
-    for (i=0; i<50; i++){
-    if(bank[0][i]==0){
-    bank[0][i]=i+901;
-    bank[1][i]=1;
-    bank[2][i]=amount;
-    printf("New account number is: %d\n",i+901);
-    return;
-    }
-}
-printf("your account cannot be opened!");    
-}    
+void openAccount() {
+    double amount;
+    printf("Please enter amount for deposit: \n");
+    scanf("%lf", &amount);
 
-void checkBalance(int accountNum){
-    for (int i = 0; i < 50; i++){
-    if(bank[1][i]!=1){
-    bank[1][i]=0;
+    if(amount > 0)
+    {
+    int i;
+    for (i = 0; i < 50; i++)
+    {
+        if (bank[1][i] != 1)
+        {
+            bank[0][i] = i + 901;
+            bank[1][i] = 1;
+            bank[2][i] = amount;
+            printf("New account number is: %d\n", i + 901);
         }
     }
-        if((accountNum>950)||(accountNum<900)){
-            printf("illeagle account");
+    }
+    else
+    {
+        printf("Invalid amount\n");
+        return;
+    }
+    
+    printf("Failed to read the amount\n");
+}
+
+void checkBalance() {
+    int accountNum;
+    printf("Please enter account number: \n");
+    scanf("%d", &accountNum);
+
+    for (int i = 0; i < 50; i++)
+    {
+        if (bank[1][i] != 1)
+        {
+            bank[1][i] = 0;
+        }
+    }
+    if ((accountNum > 950) || (accountNum < 900))
+    {
+        printf("illegal account");
+        return;
+    }
+    if (bank[0][accountNum - 901] == accountNum)
+    {
+        if (bank[1][accountNum - 901] == 1)
+        {
+            printf("The balance of account number %d is : %.2lf \n", accountNum, bank[2][accountNum - 901]);
             return;
-        } 
-        if(bank[0][accountNum-901]==accountNum){
-            if(bank[1][accountNum-901]==1){
-            printf("The balance of account number %d is : %.2lf \n",accountNum, bank[2][accountNum-901]);
-            return;
-            }
-            if(bank[1][accountNum-901]== 0){
-            printf("something wrong with your account its close.\n");
+        }
+        if (bank[1][accountNum - 901] == 0)
+        {
+            printf("something is wrong with your account, it's close.\n");
             return;
         }
     }
     printf("no such account!\n");
 }
 
-void deposit(int accountNum){
+void deposit() {
+    int accountNum;
+    printf("Please enter account number: \n");
+    scanf("%d", &accountNum);
+
+    if ((accountNum > 950) || (accountNum < 900))
+    {
+        printf("illegal account");
+        return;
+    }
+
     double deposit;
     printf("Please enter the amount to deposit: \n");
     scanf(" %lf", &deposit);
-    if (deposit < 0) {
+    if (deposit < 0)
+    {
         printf("You can't deposit a negative number \n");
         return;
     }
-    bank[2][accountNum-901]+=deposit;
-    printf("The new balance is: %.2lf \n",bank[2][accountNum-901]);
-}   
 
-void withdraw(int accountNum){
+    if (bank[1][accountNum - 901] == 1) 
+    {
+        bank[2][accountNum - 901] += deposit;
+        printf("The new balance is: %.2lf \n", bank[2][accountNum - 901]);
+        return;
+    }
+    else
+    {
+        printf("something is wrong with your account, it's close.\n");
+        return;
+    }
+}
+
+void withdraw() {
+    int accountNum;
+    printf("Please enter account number: \n");
+    scanf("%d", &accountNum);
+    if ((accountNum > 950) || (accountNum < 900))
+    {
+        printf("illegal account");
+        return;
+    }
+
     double withdraw;
     printf("Please enter the amount to withdraw: \n");
     scanf(" %lf", &withdraw);
-    if (withdraw < 0) {
+    if (withdraw < 0)
+    {
         printf("You can't withdraw a negative number \n");
         return;
     }
-    bank[2][accountNum-901]-=withdraw;
-    printf("The new balance is: %.2lf \n",bank[2][accountNum-901]);
-}
+    
+    if (bank[1][accountNum - 901] == 1) 
+    {
+        if (withdraw < bank[2][accountNum - 901])
+        {
+        printf("Your withdraw is too large\n");
+        return;
+        }
 
-void closeAccount(int accountNum){
-    if (accountNum < 901) {
-        printf("No such account! \n");
+        bank[2][accountNum - 901] -= withdraw;
+        printf("The new balance is: %.2lf \n", bank[2][accountNum - 901]);
+        return;
     }
-
-    bank[0][accountNum-901] = 0;
-    bank[1][accountNum-901] = 0;
-    bank[2][accountNum-901] = 0;
-    printf("This account is closed \n");
+    else
+    {
+        printf("This account is closed.\n");
+        return;
+    }
+        
 }
 
-void addInterest(){
+void closeAccount()
+{
+    int accountNum;
+    printf("Please enter account number: \n");
+    scanf("%d", &accountNum);
+
+    if ((accountNum > 950) || (accountNum < 900))
+    {
+        printf("illegal account");
+        return;
+    }
+    if (bank[1][accountNum - 901] == 1) 
+    {
+        bank[0][accountNum - 901] = 0;
+        bank[1][accountNum - 901] = 0;
+        bank[2][accountNum - 901] = 0;
+        printf("This account is closed \n");
+        return;
+    }
+    else
+    {
+        printf("Your account is already closed.\n");
+        return;
+    }
+    
+}
+
+void addInterest()
+{
     double interest_rate;
     printf("Please enter interest rate: \n");
     scanf("%lf", &interest_rate);
-    
+
+    if (interest_rate < 0)
+    {
+        printf("Interest rate can't be negative\n");
+        return;
+    }
+
     for (int i = 0; i < 50; i++)
     {
-        if (interest_rate < 0) {
-            bank[2][i] -= bank[2][i]*((-interest_rate)/100);
-        }
-        bank[2][i] *= (interest_rate/100)+1;
+        bank[2][i] *= (interest_rate / 100) + 1;
     }
     printf("The new balance is increased by: %.2lf", interest_rate);
 }
 
-void printAccount(){
-    for (int i = 0; i < 50; i++){
-        if (bank[1][i]== 1){
+void printAccount()
+{
+    for (int i = 0; i < 50; i++)
+    {
+        if (bank[1][i] == 1)
+        {
             printf("account number: %d", (int)bank[0][i]);
             printf(", The balance is: %.2lf \n", bank[2][i]);
         }
-        
     }
 }
 
-void closeAccounts(){
-    for (int i = 0; i < 50; i++){
-        if (bank[1][i]== 1){
-            bank[0][i]== 0;
-            bank[1][i]== 0;
-            bank[2][i]== 0;
+void closeAccounts()
+{
+    for (int i = 0; i < 50; i++)
+    {
+        if (bank[1][i] == 1)
+        {
+            bank[0][i] = 0;
+            bank[1][i] = 0;
+            bank[2][i] = 0;
         }
     }
-        printf("All accounts are closed! \n");
+    printf("All accounts are closed! \n");
 }
 
 
