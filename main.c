54e8a3070a084e1a1904dbd64dafc7011e43c163
maoz lev <maoz20011111@gmail.com>
2020-12-05 21:45:42 +0200

    final update

--- main.c
+++ main.c
@@ -3,7 +3,6 @@
 
 int main()
 {
-<<<<<<< HEAD
   char Transaction; 
   do
   {
@@ -51,56 +50,5 @@ int main()
     }
   }
   while(Transaction!='E');
-=======
-  char Transaction = ' ';
-  
-  while (1)
-  {
-    printf("\nPlease choose a transaction type: \n");
-    printf(" O-Open Account\n B-Balance Inquiry\n D-Deposit\n W-Withdraw\n C-Close Account\n I-Interest\n P-Print\n E-Exit\n");
-    scanf(" %c", &Transaction);
-
-    switch(Transaction) {
-
-    case 'O':
-      openAccount();
-      break;
-
-    case 'B':
-      checkBalance();
-      break;
-
-    case 'D':
-      deposit();
-      break;
-
-    case 'W':
-      withdraw();
-      break;
-
-    case 'C':
-      closeAccount();
-      break;
-
-    case 'I':
-      addInterest();
-      break;
-
-    case 'P':
-      printAccount();
-      break;
-
-    case 'E':
-      closeAccounts();
-      return 0;
-      break;
-
-    default:
-      printf("invalid command!\n");
-      break;
-
-    }
-  }
->>>>>>> ea3d5ae349c07b6c085f213eb27afc30c1e5be16
   return 0;
 }
